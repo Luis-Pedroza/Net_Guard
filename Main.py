ef56309566446e368ee26278e8ac3094e97e4102
Luis-Pedroza <pedroza.moranchel@hotmail.com>
2023-08-29 19:14:18 -0600

    style: aplied PEP8 to Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 # ***************************************************
 # FILE: Main.py
 #
-# DESCRIPTION: 
+# DESCRIPTION:
 # This code imports the main UI and initialize it
 #
 # AUTHOR:  Luis Pedroza
@@ -9,24 +9,27 @@
 # ******************* ********************************
 
 # Command to create an exe.
-# pyinstaller --onedir --noconsole --icon=Resources\icon.ico --uac-admin --add-data "Resources;Resources" -n "Net Guard" Main.py
+# pyinstaller --onedir --noconsole \
+# --icon=Resources\icon.ico --uac-admin \
+#     --add-data "Resources;Resources" -n "Net Guard" Main.py
 
 
 import sys
 from PyQt5 import QtWidgets
 from UI_Module.MainWindow import Ui_MainWindow
 
-#Initialize the Ui_MainWindow as a QMainWindow type
+
+# Initialize the Ui_MainWindow as a QMainWindow type
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-#Initialize de mainWindow until program is closed
-if __name__ == '__main__':
 
-    InitMainWindow = QtWidgets.QApplication([])    
+# Initialize de mainWindow until program is closed
+if __name__ == '__main__':
+    InitMainWindow = QtWidgets.QApplication([])
     MainApp = MainWindow()
     MainApp.show()
     sys.exit(InitMainWindow.exec())
