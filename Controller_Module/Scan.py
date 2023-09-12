9e3a9ca4066267ca1fc5f0421557938d0d580b3b
Luis-Pedroza <pedroza.moranchel@hotmail.com>
2023-09-12 11:47:48 -0600

    Docs: Added documentation to Scan.py

--- Controller_Module/Scan.py
+++ Controller_Module/Scan.py
@@ -1,9 +1,12 @@
 # ***************************************************
 # FILE: Scan.py
 #
-# DESCRIPTION: 
-# This code does a ports scan and returns the obtained data
-# It also gets the range of the ports and changes it
+# DESCRIPTION:
+# The program encapsulates functionality for managing network ports and port ranges.
+# It allows users to scan and retrieve information about active network connections,
+# such as protocol, local and remote addresses, status, process IDs, and program names.
+# Additionally, it provides features to query and modify the range of dynamic ports
+# for TCP and UDP protocols.
 #
 # AUTHOR:  Luis Pedroza
 # CREATED: 16/03/2023 (dd/mm/yy)
@@ -16,86 +19,145 @@ from UI_Module.UI_Message import PopUpMessage
 
 
 class ScanPorts():
+    """
+    A class for scanning and managing active network ports and port ranges.
+
+    Attributes:
+        message (PopUpMessage): A class for displaying pop-up messages.
+        icon (QMessageBox.Icon): An icon for message boxes in case of errors.
+
+    Methods:
+        scan_active_ports():
+            Static method to retrieve information about all active network connections,
+            including protocol, local and remote addresses, status, process ID, and program name.
+
+        get_ports_range():
+            Method to retrieve the current range of dynamic ports for TCP and UDP protocols.
+            It uses system commands to get the port range information.
+
+        change_ports_range(protocol: str, range: int) -> bool:
+            Method to change the range of dynamic ports for a specified protocol (TCP or UDP).
+            It uses a system command to modify the port range.
+
+    """
     def __init__(self):
+        """
+        Initializes the ScanPorts class.
+
+        Attributes:
+            message (PopUpMessage): A class for displaying pop-up messages.
+            icon (QMessageBox.Icon): An icon for message boxes in case of errors.
+
+        """
         self.message = PopUpMessage()
         self.icon = QMessageBox.Critical
-    # static method to get all the ports in use
+
     @staticmethod
-    def scan_active_ports():
-        # initialize a connection of all kinds (TCP & UDP)
-        # and an empty list
+    def scan_active_ports() -> list:
+        """
+        Static method to retrieve information about all active network connections.
+
+        Args:
+            None
+
+        Returns:
+            list: A list of lists containing connection data, including protocol,
+                  local address, remote address, status, process ID, and program name.
+
+        Raises:
+            - Displays a pop-up message if there are errors while retrieving process names.
+
+        Example Usage:
+            active_ports = ScanPorts.scan_active_ports()
+            for port_info in active_ports:
+                print(port_info)
+
+        """
         connections = psutil.net_connections(kind='all')
         connection_data = []
 
-        # gets each data on the connection
         for data in connections:
-            # get the local socket (ip address & port)
             local_address = f"{data.laddr.ip}:{data.laddr.port}"
-            # get the remote socket if there´s any (ip address & port)
             remote_address = f"{data.raddr.ip}:{data.raddr.port}" if data.raddr else "-"
-            # get the status of the connection
             status = data.status
-            # get the PID of the program
             pid = data.pid
             try:
-                # gets the name of the program is there is´nt an exception
                 program = psutil.Process(pid).name()
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 program = "-"
-            # gets the protocol used on the socket
             if data.type == 1:
                 protocol = "TCP"
             elif data.type == 2:
                 protocol = "UDP"
             else:
                 protocol = "-" if data.kind != psutil.SOCK_RAW else "RAW"
-            # insert the data on the list and return it
             connection_data.append([protocol, local_address, remote_address, status, pid, program])
         return connection_data
-    
-    # Method to get the range of the ports 
-    def get_ports_range(self):
-        # command to get tcp and udp range
+
+    def get_ports_range(self) -> list:
+        """
+        Static method to retrieve information about all active network connections.
+
+        Args:
+            None
+
+        Returns:
+            list: A list of lists containing connection data, including protocol,
+                  local address, remote address, status, process ID, and program name.
+
+        Raises:
+            - Displays a pop-up message if there are errors while retrieving process names.
+
+        Example Usage:
+            active_ports = ScanPorts.scan_active_ports()
+            for port_info in active_ports:
+                print(port_info)
+
+        """
         command_TCP = 'netsh int ipv4 show dynamicport tcp'
         command_UDP = 'netsh int ipv4 show dynamicport udp'
-        try: 
-            # execute both commands
+        try:
             output_TCP = subprocess.run(command_TCP, shell=True, capture_output=True, encoding='cp850')
             output_UDP = subprocess.run(command_UDP, shell=True, capture_output=True, encoding='cp850')
-            # if output has an error show an error window
             if output_TCP.returncode != 0 or output_UDP.returncode != 0:
-                self.message.show_message('UNABLE_To_get_ports_range',output_TCP.stdout.splitlines()[1], self.icon)
-            # else return the output
+                self.message.show_message('UNABLE_To_get_ports_range', output_TCP.stdout.splitlines()[1], self.icon)
             else:
                 return str(output_TCP.stdout), str(output_UDP.stdout)
-        # exception control
         except subprocess.CalledProcessError as exception:
             self.message.show_message('UNABLE_To_Execute_Command_in_get_ports_range', exception, self.icon)
 
-    # Method to change the range of the ports
-    def change_ports_range(self, protocol, range):
+    def change_ports_range(self, protocol: str, range: int) -> bool:
+        """
+        Method to change the range of dynamic ports for a specified protocol (TCP or UDP).
+
+        Args:
+            protocol (str): The protocol for which to change the port range ('TCP' or 'UDP').
+            range (int): The new port range to set.
+
+        Returns:
+            bool: True if the port range was successfully changed; False otherwise.
+
+        Raises:
+            - Displays a pop-up message if there are errors while executing system commands.
+
+        Example Usage:
+            if scan_ports.change_ports_range('TCP', 10000):
+                print("TCP Port Range successfully changed.")
+            else:
+                print("Failed to change TCP Port Range.")
+
+        """
         status = True
         # change range with first port 49152(dynamic range). IANA recommendation default=16384
         command_range = f'netsh int ipv4 set dynamicport {protocol} start=49152 num={range}'
-        try: 
-            # execute command
+        try:
             output_range = subprocess.run(command_range, shell=True, capture_output=True, encoding='cp850')
-            # if output has an error show a popUp message and return status false
             if output_range.returncode != 0:
                 self.message.show_message('UNABLE_To_change_ports_range', output_range.stdout, self.icon)
                 status = False
-            # if output does'nt has an error return status true
             else:
                 status = True
-        # if there's an exception return code False
         except subprocess.CalledProcessError as exception:
             self.message.show_message('UNABLE_To_Execute_Command_in_change_ports_range', exception, self.icon)
             status = False
         finally: return status
-
-
-                
-
-
-
-          
